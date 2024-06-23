de29150a80aa4b108cdddbf624686f99e4cec768
Tamer <tamerchoyy@gmail.com>
2024-06-23 20:09:40 +0300

    forgot to reverse the password after the validation

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def run() -> None:
     password = input("Your password: ")
     validate_password(password)
 
-    ascii_art = convert_password_to_art(password)
+    ascii_art = convert_password_to_art(password[::-1])
     for line in ascii_art:
         print(line)
 
